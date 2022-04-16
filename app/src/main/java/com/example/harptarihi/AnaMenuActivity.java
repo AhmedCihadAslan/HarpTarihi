0cbd5f1f3fcbdf182417ca97f1b8e43db4d5adff
Ahmet Bozkan Altamira <ahmetbozkan@altamira.com.tr>
2022-04-17 00:53:38 +0300

    ana menü tasarımı

---
+++ app/src/main/java/com/example/harptarihi/AnaMenuActivity.java
@@ -0,0 +1,32 @@
+package com.example.harptarihi;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.harptarihi.databinding.ActivityAnaMenuBinding;
+
+public class AnaMenuActivity extends AppCompatActivity {
+
+    private ActivityAnaMenuBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityAnaMenuBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        binding.imgOyna.setOnClickListener(v -> {
+            Intent intent = new Intent(AnaMenuActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        binding.imgSiteyeGit.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.harptarihi.net/"));
+            startActivity(intent);
+        });
+    }
+}
