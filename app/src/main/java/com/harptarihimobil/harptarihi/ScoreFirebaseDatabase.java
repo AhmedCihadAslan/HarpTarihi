d6a5bdddd91d20b17fab9fd7130673e6d04f2436
Ahmet Bozkan <ahmetbozkan@outlook.com>
2022-09-10 20:25:34 +0300

    ayarlamalar

--- app/src/main/java/com/harptarihimobil/harptarihi/ScoreFirebaseDatabase.java
+++ app/src/main/java/com/harptarihimobil/harptarihi/ScoreFirebaseDatabase.java
@@ -1,17 +1,14 @@
 package com.harptarihimobil.harptarihi;
 
 import android.content.Context;
-import android.widget.Toast;
+import android.util.Log;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 public class ScoreFirebaseDatabase {
 
@@ -30,7 +27,17 @@ public class ScoreFirebaseDatabase {
     public void setScore(int score) {
         String username = sharedPrefs.getUsername();
 
-        db.child(username).setValue(score);
+        db.child(username).setValue(score).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Log.d("TAG", "onSuccess: ");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("TAG", "onFailure: " + e);
+            }
+        });
     }
 
 
